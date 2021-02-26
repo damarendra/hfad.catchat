e4c32024ebe3d8dc98e39117cadf3a122420b92f
damarendra <damarendran@gmail.com>
2021-02-26 20:21:43 +0700

    + added inbox, sent, and trash fragment +added toolbar_main and modified app theme

---
+++ app/src/main/java/com/example/catchat/fragments/SentItemsFragment.java
@@ -0,0 +1,27 @@
+package com.example.catchat.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.catchat.R;
+
+public class SentItemsFragment extends Fragment {
+
+    public SentItemsFragment() { }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sent_items, container, false);
+    }
+}
