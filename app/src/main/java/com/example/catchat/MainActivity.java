75d9f0d00f82427e0b158309089256f29e59fbc5
damarendra <damarendran@gmail.com>
2021-02-27 19:57:56 +0700

    set drawer layout and navigation view

--- app/src/main/java/com/example/catchat/MainActivity.java
+++ app/src/main/java/com/example/catchat/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.catchat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.example.catchat.fragments.InboxFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewById(R.id.toolbar_main));
+
+        Fragment inboxFragment = new InboxFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.main_content, inboxFragment);
+        ft.commit();
     }
 }
